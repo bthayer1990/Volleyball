b266d7aaa483c5fc3f96d832c68f2e460f794e00
bthayer1990 <bthayer1990@gmail.com>
2017-03-04 10:49:45 -0600

    navigate to match activity from main on button click

--- app/src/main/java/com/bnb/volleyball/MainActivity.java
+++ app/src/main/java/com/bnb/volleyball/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bnb.volleyball;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startMatch(View view) {
+        Intent intent = new Intent(this, MatchActivity.class);
+        startActivity(intent);
     }
 }
